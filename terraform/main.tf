c43879d355f6eec6a15803148bd0dd0d80dd31a5
cross-web <avi070440@gmail.com>
2025-06-27 16:10:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,6 @@ resource "kubernetes_namespace" "todo-app" {
 resource "helm_release" "flask" {
   name             = "flask"
   chart            = "../helm-chart"
-  namespace        = kubernetes_namespace.flask.metadata[0].name
+  namespace        = kubernetes_namespace.todo-app.metadata[0].name
   create_namespace = false
 }
